38bf7e78de69c660e1764425f7221f7321f1337d
Charles Mariot <contact@charles-mariot.fr>
2023-05-28 21:41:30 +0200

    ML Module 00 : Python matrix class

---
+++ module00/ex00/matrix.py
@@ -0,0 +1,172 @@
+class Matrix:
+
+    # The matrix object can be initialized with either:
+    # - the elements of the matrix as a list of lists
+    #   Matrix([[1.0, 2.0], [3.0, 4.0]])
+    # - the shape of the matrix as a tuple
+    #   Matrix((2, 2))
+    def __init__(self,
+                 data: 'list[list[float]]' = None,
+                 shape: 'tuple[int, int]' = None):
+
+        if data is None and shape is None:
+            raise TypeError("Matrix() missing 1 required argument" +
+                            ": 'data' or 'shape'")
+
+        elif data is not None and shape is not None:
+            raise TypeError("Matrix() takes 1 positional argument" +
+                            " but 2 were given")
+
+        elif data is not None:
+            if (not isinstance(data, list)
+                    or not all(isinstance(x, list) for x in data)):
+                raise TypeError("Data must be a list of lists")
+            elif not all(len(x) == len(data[0]) for x in data):
+                raise ValueError("Data must be a matrix," +
+                                 " all rows must have the same length")
+            elif not all(
+                    isinstance(x, (int, float)) for row in data for x in row):
+                raise TypeError("Data must contain only integers or floats")
+            self.data = [[float(x) for x in row] for row in data]
+            self.shape = (len(data), len(data[0]))
+
+        elif shape is not None:
+            if not isinstance(shape, tuple):
+                raise TypeError("Shape must be a tuple")
+            elif len(shape) != 2:
+                raise ValueError("Shape must be a tuple of length 2")
+            elif not all(isinstance(x, int) for x in shape):
+                raise TypeError("Shape must contain only integers")
+            elif not all(x > 0 for x in shape):
+                raise ValueError("Shape must contain only positive integers")
+            self.data = [[0.0 for _ in range(shape[1])]
+                         for _ in range(shape[0])]
+            self.shape = shape
+
+    # add : only matrices of same dimensions.
+    # __add__
+    def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError("Can only add a Matrix to a Matrix")
+        elif self.shape != other.shape:
+            raise ValueError("Can only add matrices of same shape")
+
+        return Matrix([[a + b for a, b in zip(x, y)]
+                       for x, y in zip(self.data, other.data)])
+
+    # __radd__
+    def __radd__(self, other):
+        return self + other
+
+    # sub : only matrices of same dimensions.
+    # __sub__
+    def __sub__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError("Can only subtract a Matrix from a Matrix")
+        elif self.shape != other.shape:
+            raise ValueError("Can only subtract matrices of same shape")
+
+        return Matrix([[a - b for a, b in zip(x, y)]
+                       for x, y in zip(self.data, other.data)])
+
+    # __rsub__
+    def __rsub__(self, other):
+        return self - other
+
+    # div : only scalars.
+    # __truediv__
+    def __truediv__(self, other):
+        if not isinstance(other, (int, float)):
+            raise TypeError("Can only divide a Matrix by a scalar")
+        elif other == 0:
+            raise ZeroDivisionError("Cannot divide a Matrix by 0")
+        return self * (1 / other)
+
+    # __rtruediv__
+    def __rtruediv__(self, other):
+        if not isinstance(other, (int, float)):
+            raise TypeError("Can only divide a scalar by a Matrix")
+        elif any(0 in row for row in self.data):
+            raise ZeroDivisionError(
+                "Cannot divide by a Matrix with a 0 element")
+        return Matrix([[other / a for a in row] for row in self.data])
+
+    # mul : scalars, vectors and matrices,
+    # can have errors with vectors and matrices,
+    # returns a Vector if we perform Matrix * Vector mutliplication.
+    # __mul__
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            if self.shape[1] != other.shape[0]:
+                raise ValueError("Can only multiply matrices" +
+                                 " if the number of columns of the first" +
+                                 " matrix is equal to the number of rows" +
+                                 " of the second matrix")
+            return Matrix([[sum(a * b for a, b in zip(x, y))
+                            for y in zip(*other.data)]
+                           for x in self.data])
+        elif isinstance(other, Vector):
+            if self.shape[1] != other.shape[0]:
+                raise ValueError("Can only multiply a matrix by a vector" +
+                                 " if the number of columns of the matrix" +
+                                 " is equal to the number of rows of" +
+                                 " the vector")
+            return Vector([sum(a * b for a, b in zip(x, other.data))
+                           for x in self.data])
+        elif isinstance(other, (int, float)):
+            return Matrix([[a * other for a in x] for x in self.data])
+        else:
+            raise TypeError("Can only multiply a Matrix by a scalar," +
+                            " a Vector or a Matrix")
+
+    # __rmul__
+    def __rmul__(self, other):
+        return self * other
+
+    # __str__
+    def __str__(self):
+        return str(self.data)
+
+    # __repr__
+    def __repr__(self):
+        return str(self.data)
+
+    # Transpose the matrix
+    def T(self):
+        new = Matrix([[a for a in x] for x in zip(*self.data)])
+        self.data = new.data
+        self.shape = new.shape
+        return self
+
+    # ==
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Matrix):
+            return (self.data == other.data
+                    and self.shape == other.shape)
+        return False
+
+
+class Vector(Matrix):
+
+    def __init__(self, data):
+        super().__init__(data, (len(data), 1))
+
+    # add : only vectors of same dimensions.
+    # __add__
+    # __radd__
+
+    # sub : only vectors of same dimensions.
+    # __sub__
+    # __rsub__
+
+    # div : only scalars.
+    # __truediv__
+    # __rtruediv__
+
+    # mul : scalars, vectors and matrices,
+    # can have errors with vectors and matrices,
+    # returns a Vector if we perform Matrix * Vector mutliplication.
+    # __mul__
+    # __rmul__
+    # __str__
+    # __repr__
---
+++ module00/ex00/test.py
@@ -0,0 +1,208 @@
+import unittest
+from matrix import Matrix
+
+
+class TestMatrix(unittest.TestCase):
+
+    def setUp(self):
+        self.matrix1 = Matrix([[1, 2], [3, 4]])
+        self.matrix2 = Matrix([[5, 6], [7, 8]])
+        self.scalar1 = 2
+
+    def test_init_with_data(self):
+        # Test initializing matrix with data
+        data = [[1, 2], [3, 4]]
+        matrix = Matrix(data=data)
+        self.assertEqual(matrix.data, data)
+        self.assertEqual(matrix.shape, (2, 2))
+
+        # Test initializing matrix with invalid data
+        with self.assertRaises(TypeError):
+            Matrix(data="invalid")
+
+        with self.assertRaises(ValueError):
+            Matrix(data=[[1, 2], [3]])
+
+        with self.assertRaises(TypeError):
+            Matrix(data=[[1, 2], [3, "invalid"]])
+
+    def test_init_with_shape(self):
+        # Test initializing matrix with shape
+        shape = (2, 2)
+        matrix = Matrix(shape=shape)
+        self.assertEqual(matrix.data, [[0.0, 0.0], [0.0, 0.0]])
+        self.assertEqual(matrix.shape, shape)
+
+        # Test initializing matrix with invalid shape
+        with self.assertRaises(TypeError):
+            Matrix(shape="invalid")
+
+        with self.assertRaises(TypeError):
+            Matrix(shape=(2, "invalid"))
+
+        with self.assertRaises(ValueError):
+            Matrix(shape=(-1, 2))
+
+    def test_add(self):
+        # Test adding matrices of same dimensions
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        matrix2 = Matrix(data=[[5, 6], [7, 8]])
+        result = matrix1 + matrix2
+        self.assertEqual(result.data, [[6, 8], [10, 12]])
+        self.assertEqual(result.shape, (2, 2))
+
+        # Test adding matrices of different dimensions
+        matrix3 = Matrix(data=[[1, 2], [3, 4]])
+        matrix4 = Matrix(data=[[5, 6, 7], [8, 9, 10]])
+        with self.assertRaises(ValueError):
+            matrix3 + matrix4
+
+    def test_add_with_scalar(self):
+        # Test adding a scalar to a matrix
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        with self.assertRaises(TypeError):
+            matrix1 + 2
+
+        # Test adding a scalar to an invalid type
+        with self.assertRaises(TypeError):
+            matrix1 + "invalid"
+
+    def test_radd(self):
+        # Test right addition with a matrix
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        matrix2 = Matrix(data=[[2, 3], [4, 5]])
+        result = matrix2 + matrix1
+        self.assertEqual(result.data, [[3, 5], [7, 9]])
+        self.assertEqual(result.shape, (2, 2))
+
+        # Test right addition with a scalar
+        with self.assertRaises(TypeError):
+            2 + matrix1
+
+        # Test right addition with an invalid type
+        with self.assertRaises(TypeError):
+            "invalid" + matrix1
+
+    def test_sub(self):
+        # Test subtracting matrices of same dimensions
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        matrix2 = Matrix(data=[[5, 6], [7, 8]])
+        result = matrix1 - matrix2
+        self.assertEqual(result.data, [[-4, -4], [-4, -4]])
+        self.assertEqual(result.shape, (2, 2))
+
+        # Test subtracting matrices of different dimensions
+        matrix3 = Matrix(data=[[1, 2], [3, 4]])
+        matrix4 = Matrix(data=[[5, 6, 7], [8, 9, 10]])
+        with self.assertRaises(ValueError):
+            matrix3 - matrix4
+
+        # Test subtracting an invalid type
+        with self.assertRaises(TypeError):
+            matrix1 - "invalid"
+
+    def test_rsub(self):
+        # Test right subtraction with a matrix
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        matrix2 = Matrix(data=[[2, 3], [4, 5]])
+        result = matrix2 - matrix1
+        self.assertEqual(result.data, [[1, 1], [1, 1]])
+        self.assertEqual(result.shape, (2, 2))
+
+        # Test right subtraction with a scalar
+        with self.assertRaises(TypeError):
+            2 - matrix1
+
+        # Test right subtraction with an invalid type
+        with self.assertRaises(TypeError):
+            "invalid" - matrix1
+
+    def test_mul(self):
+
+        self.matrix1 = Matrix([[1, 2], [3, 4]])
+        self.matrix2 = Matrix([[5, 6], [7, 8]])
+        self.scalar1 = 2
+
+        # Test matrix * matrix multiplication
+        result = self.matrix1 * self.matrix2
+        expected = Matrix([[19, 22], [43, 50]])
+        self.assertEqual(result, expected)
+
+        # Test matrix * scalar multiplication
+        result = self.matrix1 * self.scalar1
+        expected = Matrix([[2, 4], [6, 8]])
+        self.assertEqual(result, expected)
+
+        # Test invalid input type
+        with self.assertRaises(TypeError):
+            self.matrix1 * "string"
+
+        # Test invalid matrix dimensions
+        print(self.matrix1 * self.matrix1.T())
+
+    def test_rmul(self):
+        # Test scalar * matrix multiplication
+        result = self.scalar1 * self.matrix1
+        expected = Matrix([[2, 4], [6, 8]])
+        self.assertEqual(result, expected)
+
+        # Test invalid input type
+        with self.assertRaises(TypeError):
+            "string" * self.matrix1
+
+    def test_div(self):
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        result = matrix1 / 2
+        expected = Matrix([[0.5, 1.0], [1.5, 2.0]])
+        self.assertEqual(result.shape, expected.shape)
+        self.assertEqual(result.data, expected.data)
+
+        with self.assertRaises(TypeError):
+            matrix2 = Matrix(data=[[1.0, 2.0], [3, 4]])
+            matrix1 / matrix2
+
+        with self.assertRaises(TypeError):
+            matrix1 / "invalid"
+
+        with self.assertRaises(ZeroDivisionError):
+            self.matrix1 / 0
+
+    def test_rdiv(self):
+        matrix1 = Matrix(data=[[1, 2], [3, 4]])
+        result = 2 / matrix1
+        expected = Matrix([[2.0, 1.0], [2.0 / 3, 0.5]])
+        self.assertEqual(result.shape, expected.shape)
+        self.assertEqual(result.data, expected.data)
+
+        with self.assertRaises(TypeError):
+            "invalid" / matrix1
+
+        with self.assertRaises(ZeroDivisionError):
+            matrix2 = Matrix([[0, 1.0], [2.0 / 3, 0.5]])
+            2 / matrix2
+
+    def test_T(self):
+
+        matrix1 = Matrix([[1, 2], [3, 4]])
+        matrix1.T()
+        expected = Matrix([[1.0, 3.0], [2.0, 4.0]])
+        self.assertEqual(matrix1, expected)
+
+        matrix2 = Matrix([[5, 6, 7], [8, 9, 10]])
+        matrix2.T()
+        expected = Matrix([[5, 8], [6, 9], [7, 10]])
+        self.assertEqual(matrix2, expected)
+
+        matrix3 = Matrix([[1, 2], [3, 4], [5, 6]])
+        matrix3.T()
+        expected = Matrix([[1, 3, 5], [2, 4, 6]])
+        self.assertEqual(matrix3, expected)
+
+        matrix4 = Matrix([[1]])
+        matrix4.T()
+        expected = Matrix([[1]])
+        self.assertEqual(matrix4, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
