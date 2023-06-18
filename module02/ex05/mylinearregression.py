aecfa7e450cee4227d3f0638892d54de6a6767df
Charles Mariot <contact@charles-mariot.fr>
2023-06-18 21:26:39 +0200

    Module02 : linear regression with multi-features

--- module02/ex05/mylinearregression.py
+++ module02/ex05/mylinearregression.py
@@ -1,23 +1,141 @@
 import numpy as np
 
 
-class MyLinearRegression():
-    """
-        Description:
-            My personnal linear regression class to fit like a boss.
-    """
+class MyLR:
 
     def __init__(self, thetas, alpha=0.001, max_iter=1000):
-        pass
-
-    def fit_(self, x, y):
-        pass
+        if not isinstance(alpha, float) or alpha < 0.0:
+            print("Error 2")
+            return None
+        elif not isinstance(max_iter, int) or max_iter < 0:
+            print("Error 3")
+            return None
+        self.thetas = np.array(thetas)
+        self.alpha = alpha
+        self.max_iter = max_iter
+        print("OK")
 
     def predict_(self, x):
-        pass
+        if not isinstance(x, np.ndarray):
+            return None
+        elif x.size == 0:
+            return None
+        m = x.shape[0]
+        n = x.shape[1]
+        if self.thetas.shape != (n + 1, 1):
+            return None
+        X_prime = np.concatenate((np.ones((m, 1)), x), axis=1)
+        y_hat = np.dot(X_prime, self.thetas)
+        return y_hat
 
     def loss_elem_(self, y, y_hat):
-        pass
+        for arr in [y, y_hat]:
+            if not isinstance(arr, np.ndarray):
+                return None
+        m = y.shape[0]
+        if m == 0:
+            return None
+        if y.shape != (m, 1) or y_hat.shape != (m, 1):
+            return None
+        J_elem = np.square(y_hat - y)
+        return J_elem
 
     def loss_(self, y, y_hat):
-        pass
+        J_elem = self.loss_elem_(y, y_hat)
+        if J_elem is None:
+            return None
+        J_value = np.mean(J_elem) / 2
+        return J_value
+
+    def gradient_(self, x, y):
+        for array in [x, y]:
+            if not isinstance(array, np.ndarray):
+                return None
+        m, n = x.shape
+        if m == 0 or n == 0:
+            return None
+        elif y.shape != (m, 1):
+            return None
+        elif self.thetas.shape != (n + 1, 1):
+            return None
+        X_prime = np.c_[np.ones(m), x]
+        return (1 / m) * (X_prime.T.dot(X_prime.dot(self.thetas) - y))
+
+    def fit_(self, x, y):
+        for arr in [x, y]:
+            if not isinstance(arr, np.ndarray):
+                return None
+        m, n = x.shape
+        if m == 0 or n == 0:
+            return None
+        if y.shape != (m, 1):
+            return None
+        elif self.thetas.shape != ((n + 1), 1):
+            return None
+        for _ in range(self.max_iter):
+            gradient = self.gradient_(x, y)
+            if gradient is None:
+                return None
+            if all(__ == 0. for __ in gradient):
+                break
+            self.thetas = self.thetas - self.alpha * gradient
+        return self.thetas
+
+
+if __name__ == "__main__":
+
+    X = np.array([[1., 1., 2., 3.],
+                  [5., 8., 13., 21.],
+                  [34., 55., 89., 144.]])
+
+    Y = np.array([[23.],
+                  [48.],
+                  [218.]])
+
+    mylr = MyLR([[1.],
+                 [1.],
+                 [1.],
+                 [1.],
+                 [1]])
+
+    # Example 0:
+    y_hat = mylr.predict_(X)
+    print(y_hat)
+    # Output:
+    # array([[8.], [48.], [323.]])
+
+    # Example 1:
+    mylr.loss_elem_(Y, y_hat)
+    # Output:
+    # array([[225.], [0.], [11025.]])
+
+    # Example 2:
+    loss = mylr.loss_(Y, y_hat)
+    print(loss)
+    # Output:
+    # 1875.0
+
+    # Example 3:
+    mylr.alpha = 1.6e-4
+    mylr.max_iter = 200000
+    mylr.fit_(X, Y)
+    print(mylr.thetas)
+    # Output:
+    # array([[18.188..], [2.767..], [-0.374..], [1.392..], [0.017..]])
+
+    # Example 4:
+    y_hat = mylr.predict_(X)
+    print(y_hat)
+    # Output:
+    # array([[23.417..], [47.489..], [218.065...]])
+
+    # Example 5:
+    mylr.loss_elem_(Y, y_hat)
+    # Output:
+    # array([[0.174..], [0.260..], [0.004..]])
+
+    # Example 6:
+    loss = mylr.loss_(Y, y_hat)
+    print(loss)
+    # Output:
+    # 0.0732..
---
+++ module02/ex06/multivariate_linear_model.py
@@ -0,0 +1,166 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class MyLR:
+
+    def __init__(self, thetas, alpha=0.0001, max_iter=500000):
+        if not isinstance(alpha, float) or alpha < 0.0:
+            return None
+        elif not isinstance(max_iter, int) or max_iter < 0:
+            return None
+        self.thetas = np.array(thetas)
+        self.alpha = alpha
+        self.max_iter = max_iter
+
+    def predict_(self, x):
+        if not isinstance(x, np.ndarray):
+            return None
+        elif x.size == 0:
+            return None
+        m = x.shape[0]
+        n = x.shape[1]
+        if self.thetas.shape != (n + 1, 1):
+            return None
+        X_prime = np.concatenate((np.ones((m, 1)), x), axis=1)
+        y_hat = np.dot(X_prime, self.thetas)
+        return y_hat
+
+    def loss_elem_(self, y, y_hat):
+        for arr in [y, y_hat]:
+            if not isinstance(arr, np.ndarray):
+                return None
+        m = y.shape[0]
+        if m == 0:
+            return None
+        if y.shape != (m, 1) or y_hat.shape != (m, 1):
+            return None
+        J_elem = np.square(y_hat - y)
+        return J_elem
+
+    def loss_(self, y, y_hat):
+        J_elem = self.loss_elem_(y, y_hat)
+        if J_elem is None:
+            return None
+        J_value = np.mean(J_elem) / 2
+        return J_value
+
+    def gradient_(self, x, y):
+        for array in [x, y]:
+            if not isinstance(array, np.ndarray):
+                return None
+        m, n = x.shape
+        if m == 0 or n == 0:
+            return None
+        elif y.shape != (m, 1):
+            return None
+        elif self.thetas.shape != (n + 1, 1):
+            return None
+        X_prime = np.c_[np.ones(m), x]
+        return (1 / m) * (X_prime.T.dot(X_prime.dot(self.thetas) - y))
+
+    def fit_(self, x, y):
+        for arr in [x, y]:
+            if not isinstance(arr, np.ndarray):
+                return None
+        m, n = x.shape
+        if m == 0 or n == 0:
+            return None
+        if y.shape != (m, 1):
+            return None
+        elif self.thetas.shape != ((n + 1), 1):
+            return None
+        for _ in range(self.max_iter):
+            gradient = self.gradient_(x, y)
+            if gradient is None:
+                return None
+            elif all(__ == 0. for __ in gradient):
+                break
+            self.thetas = self.thetas - self.alpha * gradient
+            print("{:2.2f} %".format(_ / self.max_iter * 100), end="\r")
+        return self.thetas
+
+    def mse_elem(self, y, y_hat) -> np.ndarray:
+        return (y_hat - y) ** 2
+
+    def mse_(self, y, y_hat) -> float:
+        if any(not isinstance(_, np.ndarray) for _ in [y, y_hat]):
+            return None
+        m = y.shape[0]
+        if m == 0 or y.shape != (m, 1) or y_hat.shape != (m, 1):
+            return None
+        J_elem = self.mse_elem(y, y_hat)
+        return J_elem.mean()
+
+
+if __name__ == "__main__":
+
+    # Instanciate linear regression class
+    linear_regression = MyLR([[0.0], [1.0]])
+
+    # Open and read the file spacecraft_data.csv with pandas
+    spacecraft_data = pd.read_csv("../ressources/spacecraft_data.csv")
+
+    # PART ONE : Univariate linear regression
+    features = ["Age", "Thrust_power", "Terameters"]
+    for feature in features:
+        linear_regression.thetas = np.array([[500.0], [1.0]])
+        x = np.array(spacecraft_data[feature]).reshape(-1, 1)
+        y = np.array(spacecraft_data["Sell_price"]).reshape(-1, 1)
+        linear_regression.fit_(x, y)
+        y_hat = linear_regression.predict_(x)
+
+        # print MSE
+        print("MSE : {}".format(linear_regression.mse_(y, y_hat)))
+
+        # Plot the data and the prediction
+        plt.scatter(x, y)
+        plt.scatter(x, y_hat, color="orange")
+        plt.title(
+            "Sell price of a spacecraft depending on its {}".format(feature))
+        plt.xlabel(feature)
+        plt.ylabel("Sell price")
+        plt.grid()
+        plt.legend(["Data", "Prediction"])
+        plt.show()
+
+    # PART TWO : Multivariate linear regression
+    features = ["Age", "Thrust_power", "Terameters"]
+    x = np.array(spacecraft_data[features])
+    y = np.array(spacecraft_data["Sell_price"]).reshape(-1, 1)
+
+    # Print MSE with initial thetas (1.0)
+    linear_regression.thetas = np.array([[1.0], [1.0], [1.0], [1.0]])
+    y_hat = linear_regression.predict_(x)
+    print("MSE : {}".format(linear_regression.mse_(y, y_hat)))
+
+    # Update thetas with appropriate values
+    linear_regression.thetas = np.array([[385.21139513],
+                                         [-24.33149116],
+                                         [5.67045772],
+                                         [-2.66684314]])
+    linear_regression.alpha = 1e-5
+    linear_regression.max_iter = 900000
+
+    # Train the model and predict y_hat
+    linear_regression.fit_(x, y)
+    y_hat = linear_regression.predict_(x)
+    print("thetas : {}".format(linear_regression.thetas))
+
+    # Plot the data and the prediction
+    for feature in features:
+        plt.scatter(x[:, features.index(feature)], y)
+        plt.scatter(x[:, features.index(feature)], y_hat,
+                    color="orange",
+                    marker=".")
+        plt.title(
+            "Sell price of a spacecraft depending on its {}".format(feature))
+        plt.xlabel(feature)
+        plt.ylabel("Sell price")
+        plt.grid()
+        plt.legend(["Data", "Prediction"])
+        plt.show()
+
+    # PART THREE : Mean squared error
+    print("MSE : {}".format(linear_regression.mse_(y, y_hat)))
