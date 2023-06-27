c5b10a03bbc3e35daed339a4fd0d99a0734285d2
Charles Mariot <contact@charles-mariot.fr>
2023-06-27 15:46:11 +0200

    Ca marche a peu pres mais tous les modeles ne sont pas la

--- module02/ex10/space_avocado.py
+++ module02/ex10/space_avocado.py
@@ -100,7 +100,7 @@ class MyLR:
             print(bar, end=print_end)
             yield item
 
-    def fit_(self, x, y, convergence=10e-5):
+    def fit_(self, x, y, convergence=10e-10):
         for arr in [x, y]:
             if not isinstance(arr, numpy.ndarray):
                 return None
@@ -140,6 +140,22 @@ class MyLR:
         if x.size == 0:
             return None
         return (x - numpy.min(x)) / (numpy.max(x) - numpy.min(x))
+
+    def normalize(self, x):
+        if not isinstance(x, numpy.ndarray):
+            return None
+        if x.size == 0:
+            return None
+        self.mean = numpy.mean(x)
+        self.std = numpy.std(x)
+        return (x - self.mean) / self.std
+
+    def denormalize(self, x):
+        if not isinstance(x, numpy.ndarray):
+            return None
+        if x.size == 0:
+            return None
+        return (x + self.mean) * self.std
 
 
 def get_dataset():
@@ -279,115 +295,67 @@ if __name__ == "__main__":
     dataset = get_dataset()
 
     # Split the dataset into a training and a test set
-    splitted = data_spliter(dataset, 0.1)
+    splitted = data_spliter(dataset, 0.5)
     if splitted is None:
         exit(1)
     else:
         (x_train, x_test, y_train, y_test) = splitted
 
+    max_degree = 4
     features = dataset.columns.values[:-1]
     nb_features = len(features)
-    max_degree = 4
 
-    # Use your polynomial_features method on your training set.
-    # For each feature, create a polynomial of 4 degrees.
+    linear_regression = MyLR(numpy.array((2, 1)), 0.001, 1_000_000)
 
-    linear_regression = MyLR(numpy.array((2, 1)), 5e-4, 1_000_000)
-    x_train_poly = []
-    for feature in range(nb_features):
-        polynomial_feature = add_polynomial_features(
-            x_train[:, feature], max_degree)
-        # Normalize the training data
+    # Normalize the features
+    for feature_index in range(nb_features):
+        x_train[:, feature_index] = \
+            linear_regression.normalize(x_train[:, feature_index])
+        x_test[:, feature_index] = \
+            linear_regression.normalize(x_test[:, feature_index])
+
+    for feature_index in range(nb_features):
+
+        fix, ax = plt.subplots(1, 4)
+        costs = []
+
         for degree in range(max_degree):
-            polynomial_feature[:, degree] = \
-                linear_regression.minmax(polynomial_feature[:, degree])
-        x_test[:, feature] = linear_regression.minmax(x_test[:, feature])
-        x_train_poly.append(polynomial_feature)
 
-    # Consider several Linear Regression models with polynomial hypothesis
-    # with a maximum degree of 4.
-    for degree in range(max_degree):
-        print("Degree : {}".format(degree + 1))
-        # Train your models on the training set.
-        x = numpy.array([x_train_poly[0][:, degree],
-                         x_train_poly[1][:, degree],
-                         x_train_poly[2][:, degree]]).T
-        y = y_train
-        if degree == 0:
-            thetas = numpy.array([[516625.14931463],
-                                  [280804.06613411],
-                                  [-26029.32304272],
-                                  [3264.76650934]])
-        elif degree == 1:
-            thetas = numpy.array([[5.54616088e+05],
-                                  [2.64783155e+05],
-                                  [-6.45475077e+01],
-                                  [-1.12745948e+04]])
-        else:
-            thetas = numpy.zeros((x.shape[1] + 1, 1))
-        linear_regression.thetas = thetas
-        y_hat = linear_regression.predict_(x)
-        mse = linear_regression.mse_(y, y_hat)
-        print("MSE : {}".format(mse))
-        print("Loss : {}".format(linear_regression.loss_(y, y_hat)))
-        linear_regression.fit_(x, y)
+            # Use your polynomial_features method on your training set
+            x_train_poly = add_polynomial_features(x_train[:, feature_index],
+                                                   degree + 1)
 
-        x_test = numpy.array([x_test[:, 0], x_test[:, 1], x_test[:, 2]]).T
-        y_hat = linear_regression.predict_(x_test)
-        mse = linear_regression.mse_(y_test, y_hat)
-        print("MSE : {}".format(mse))
-        print("Loss : {}".format(linear_regression.loss_(y_test, y_hat)))
+            # Fit the model
+            linear_regression.thetas = numpy.ones((degree + 2, 1))
+            linear_regression.fit_(x_train_poly, y_train)
 
-        fig, axs = plt.subplots(1, 3)
-        y_hat_test = linear_regression.predict_(x_test)
-        for feature in range(nb_features):
-            axs[feature].scatter(x_test[:, feature], y_test)
-            axs[feature].scatter(x_test[:, feature], y_hat_test,
-                                 color='red', marker='.')
-            axs[feature].set_title(features[feature])
+            # Use the cost function to evaluate the model on the test set
+            x_test_poly = add_polynomial_features(x_test[:, feature_index],
+                                                  degree + 1)
+            y_hat = linear_regression.predict_(x_test_poly)
+            cost = linear_regression.loss_(y_hat, y_test)
+            costs.append(cost)
+            print("Cost for degree " + str(degree) + " : " + str(cost))
+
+            x_denormalized = linear_regression.denormalize(
+                x_test[:, feature_index])
+
+            ax[degree].scatter(x_denormalized, y_test, color="blue")
+            ax[degree].scatter(x_denormalized, y_hat, color="red")
+            ax[degree].set_title(
+                "Degree " + str(degree + 1) + "\n Cost : " + str(cost))
+
+        plt.suptitle(features[feature_index])
         plt.show()
 
+        # Plot the cost depending on the degree
+        plt.figure()
+        plt.bar(range(1, max_degree + 1), costs)
+        plt.xlabel("Degree")
+        plt.ylabel("Cost")
+        plt.show()
 
-#    print("x_train shape : {}".format(x_train.shape))
-#    print("y_train shape : {}".format(y_train.shape))
-#    print("x_test shape : {}".format(x_test.shape))
-#    print("y_test shape : {}".format(y_test.shape))
-#
-#    # Create a matrix, called polynomial_train_x to store the x training set,
-#    # each index coreespond to a polynomial degree
-#    # polynomial_train_x = []
-#    # for feature in range(nb_features):
-#    #     polynomial_x = add_polynomial_features(x_train[feature], 4)
-#    #     if polynomial_x is None:
-#    #         print("Error: can't add polynomial feature.")
-#    #         exit(1)
-#    #     polynomial_train_x.append(polynomial_x)
-#
-#    # print(len(polynomial_train_x))
-#    # print(polynomial_train_x[:])
-#
-#    #for power in range(max_degree):
-#
-#    #    training_x = []
-#
-#    #    for feature in range(nb_features):
-#    #        polynomial_x = polynomial_train_x[feature]
-#    #        current_degree = polynomial_x[:, power]
-#    #        training_x.append(current_degree)
-#
-#    #    new_array = numpy.array(training_x)
-#    #    print("Shape =", new_array.shape)
-#
-#    y_hat_train = linear_regression.predict_(x_train)
-#    print("MSE : {}\n".format(
-#        linear_regression.mse_(y_train, y_hat_train)))
-#
-#    linear_regression.fit_(x_train, y_train)
-#
-#    print("theta : {}".format(linear_regression.thetas))
-#
-#    y_hat_train = linear_regression.predict_(x_train)
-#    print("MSE : {}\n".format(
-#        linear_regression.mse_(y_train, y_hat_train)))
-#
-
+        print("\n\nBest degree for feature {}: ".format(
+            features[feature_index])
+              + str(costs.index(min(costs)) + 1))
+        print()
